47f1e0e21766edb3a94afce9b0a2617b91b630c1
JoseWenned <118996462+JoseWenned@users.noreply.github.com>
2024-03-06 11:30:06 -0300

    feat: component header created

---
+++ src/Components/Header/header.component.tsx
@@ -0,0 +1,20 @@
+export const Header = () => {
+    return(
+        <>
+            <header>
+
+                <div>
+                    <h1>José Wenned</h1>
+                </div>
+
+                <div>
+                    <p>Home</p>
+                    <p>Projects</p>
+                    <p>Contacts</p>
+                    <p>Digital Media</p>
+                </div>
+                
+            </header>
+        </>
+    )
+}
