80c4627c3ba76569002148d1644a9e8429805bf5
JoseWenned <118996462+JoseWenned@users.noreply.github.com>
2024-03-06 14:54:35 -0300

    feat: Created HomePage

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import './App.css'
+import { HomePage } from './Pages/HomePage/homePage.page'
 
 export const App = () => {
 
   return (
       <>
         <main>
-            
+            <HomePage/>
         </main>
       </>
   )
