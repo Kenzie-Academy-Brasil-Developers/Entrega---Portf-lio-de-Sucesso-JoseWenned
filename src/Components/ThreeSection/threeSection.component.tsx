5d0ff4e29e20d089e197890f73dedcc296c69e55
JoseWenned <118996462+JoseWenned@users.noreply.github.com>
2024-03-07 12:41:40 -0300

    feat: add links external in component ThreeSection

--- src/Components/ThreeSection/threeSection.component.tsx
+++ src/Components/ThreeSection/threeSection.component.tsx
@@ -15,21 +15,21 @@ export const ThreeSection = () => {
                             </div>
                             <h3>My phone</h3>
                             <p>I'm available for a voice chat, let's about creativity together?</p>
-                            <button type="submit">Call now</button>
+                            <a href="https://wa.me/5588981855950" target="blank"><button type="submit">Call now</button></a>
                         </li>
 
                         <li>
                             <img src="./src/assets/Email.png"/>
                             <h3>My email</h3>
                             <p>Send me an email reporting feedbacks suggestions and ideas.</p>
-                            <button type="submit">Send email now</button>
+                            <a href="mailton:wennedchaves1997@gmail.com" target="blank"><button type="submit">Send email now</button></a>
                         </li>
 
                         <li>
                             <img src="./src/assets/linkedInTwo.png"/>
                             <h3>My LinkedIn</h3>
                             <p>We can create more constant interactions as well as a sharing network.</p>
-                            <button type="submit">Go to telegram now</button>
+                            <a href="https://www.linkedin.com/in/jos%C3%A9-wenned-chaves-de-sousa-148554165/" target="blanck"><button type="submit">Go to linkedIn now</button></a>
                         </li>
                     </ul>
                 </div>
