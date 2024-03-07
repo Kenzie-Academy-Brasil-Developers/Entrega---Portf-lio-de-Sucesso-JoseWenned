93820690b614ffa057f822eed45cac78ab69a3f7
JoseWenned <118996462+JoseWenned@users.noreply.github.com>
2024-03-07 00:22:58 -0300

    feat: add image assets

--- src/Components/ThreeSection/threeSection.component.tsx
+++ src/Components/ThreeSection/threeSection.component.tsx
@@ -8,26 +8,30 @@ export const ThreeSection = () => {
                 </div>
 
                 <div>
-                    <div>
-                        <img src="./src/assets/WhatsApp.png"/>
-                        <h3>My phone</h3>
-                        <p>I'm available for a voice chat, let's about creativity together?</p>
-                        <button type="submit">Call now</button>
-                    </div>
+                    <ul>
+                        <li>
+                            <div>
+                                <img src="./src/assets/WhatsApp.png"/>
+                            </div>
+                            <h3>My phone</h3>
+                            <p>I'm available for a voice chat, let's about creativity together?</p>
+                            <button type="submit">Call now</button>
+                        </li>
 
-                    <div>
-                        <img src="./src/assets/Email.png"/>
-                        <h3>My email</h3>
-                        <p>Send me an email reporting feedbacks suggestions and ideas.</p>
-                        <button type="submit">Send email now</button>
-                    </div>
+                        <li>
+                            <img src="./src/assets/Email.png"/>
+                            <h3>My email</h3>
+                            <p>Send me an email reporting feedbacks suggestions and ideas.</p>
+                            <button type="submit">Send email now</button>
+                        </li>
 
-                    <div>
-                        <img src="./src/assets/linkedin.png"/>
-                        <h3>My LinkedIn</h3>
-                        <p>We can create more constant interactions as well as a sharing network.</p>
-                        <button type="submit">Go to telegram now</button>
-                    </div>
+                        <li>
+                            <img src="./src/assets/linkedInTwo.png"/>
+                            <h3>My LinkedIn</h3>
+                            <p>We can create more constant interactions as well as a sharing network.</p>
+                            <button type="submit">Go to telegram now</button>
+                        </li>
+                    </ul>
                 </div>
                 
             </section>
