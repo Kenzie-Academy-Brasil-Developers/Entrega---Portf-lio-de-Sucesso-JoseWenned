3ef6c5f25a07b4a6f2246626c510019a763f5f0b
JoseWenned <118996462+JoseWenned@users.noreply.github.com>
2024-03-07 00:15:58 -0300

    feat: created component ThreeSection

--- src/Pages/ContactsPage/contactPage.page.tsx
+++ src/Pages/ContactsPage/contactPage.page.tsx
@@ -1,10 +1,13 @@
 import { Footer } from "../../Components/Footer/footer.component"
 import { Header } from "../../Components/Header/header.component"
+import { ThreeSection } from "../../Components/ThreeSection/threeSection.component"
 
 export const ContactsPage = () => {
     return(
         <>
             <Header/>
+
+            <ThreeSection/>
 
             <Footer/>
         </>
