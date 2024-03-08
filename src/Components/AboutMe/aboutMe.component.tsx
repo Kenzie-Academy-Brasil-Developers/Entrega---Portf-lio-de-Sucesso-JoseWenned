55258d61a35ba8d86608760b3be95b4d4a9962d2
JoseWenned <118996462+JoseWenned@users.noreply.github.com>
2024-03-08 10:40:42 -0300

    feat: created component aboutMe

---
+++ src/Components/AboutMe/aboutMe.component.tsx
@@ -0,0 +1,17 @@
+export const AboutMe = () => {
+    return(
+        <>
+            <section>
+                <div>
+                    <img src="" alt=""/>
+                </div>
+                <div>
+                    <h1>Hello!</h1>
+                    <h2>I'm José Wenned.</h2>
+
+                    <p>Descrição</p>
+                </div>
+            </section>
+        </>
+    )
+}
