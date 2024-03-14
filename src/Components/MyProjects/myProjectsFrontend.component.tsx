1b15a271434f94559a40e7ba1a0662fe024bc406
JoseWenned <118996462+JoseWenned@users.noreply.github.com>
2024-03-14 01:04:01 -0300

    feat: just saving data

--- src/Components/MyProjects/myProjectsFrontend.component.tsx
+++ src/Components/MyProjects/myProjectsFrontend.component.tsx
@@ -1,19 +1,35 @@
 import { ListProjectsBackend } from "./ListProjectsBackend/myProjectsBackend.component"
 import { ListProjectsFrontend } from "./ListProjectsFrontend/listProjects.components"
+import { ListProjectsFullstack } from "./ListaProjectsFullstack/listProjectFullstack.component"
 
 export const MyProjects = () => {
+
     return(
         <>  
             <header>
+
                 <button>all projects</button>
                 <button>Frontend projects</button>
                 <button>Backend projects</button>
                 <button>Fullstack projects</button>
+                
             </header>
             <section>
+
                 <h2>My Projects</h2>
-                <ListProjectsFrontend/>
-                <ListProjectsBackend/>
+
+                <div>
+                    <ListProjectsFrontend/>
+                </div>
+
+                <div>
+                    <ListProjectsBackend/>
+                </div>
+
+                <div>
+                    <ListProjectsFullstack/>
+                </div>
+                
             </section>
         </>
     )
