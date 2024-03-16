480216dff7059231c95f1de51c828c6c0b010a54
JoseWenned <118996462+JoseWenned@users.noreply.github.com>
2024-03-16 02:00:16 -0300

    feat: style update component footer

--- src/Components/Footer/footer.component.tsx
+++ src/Components/Footer/footer.component.tsx
@@ -5,15 +5,17 @@ export const Footer = () => {
         <>
             <footer className={style.footer}>
 
-                <div className={style.containerPrimary}>
-                    <h2 className={style.titleThank}>Thank you!</h2>
-                    <p className={style.paragraph}>Follow me on my social metworks and lets's talk.</p>
-                </div>
+                <div className={style.containerFooter}>
+                    <div className={style.containerPrimary}>
+                        <h2 className={style.titleThank}>Thank you!</h2>
+                        <p className={style.paragraph}>Follow me on my social metworks and lets's talk.</p>
+                    </div>
 
-                <div className={style.containerSecond}>
-                    <a href={"https://www.facebook.com/profile.php?id=100004807723751"} target="blank"><img src="./src/assets/facebook.png" alt="facebook.png"/></a>
-                    <a href={"https://www.instagram.com/j.wenned?igsh=MW5henc0Z2E3eDhqag%3D%3D&utm_source=qr"} target="blank"><img src="./src/assets/instagram.png" alt="instagram.png"/></a>
-                    <a href={"https://www.linkedin.com/in/jos%C3%A9-wenned-chaves-de-sousa-148554165/"} target="blank"><img src="./src/assets/linkedin.png" alt="linkendin.png"/></a>
+                    <div className={style.containerSecond}>
+                        <a href={"https://www.facebook.com/profile.php?id=100004807723751"} target="blank"><img className={style.imageFooter} src="./src/assets/facebook.png" alt="facebook.png"/></a>
+                        <a href={"https://www.instagram.com/j.wenned?igsh=MW5henc0Z2E3eDhqag%3D%3D&utm_source=qr"} target="blank"><img className={style.imageFooter} src="./src/assets/instagram.png" alt="instagram.png"/></a>
+                        <a href={"https://www.linkedin.com/in/jos%C3%A9-wenned-chaves-de-sousa-148554165/"} target="blank"><img className={style.imageFooter} src="./src/assets/linkedin.png" alt="linkendin.png"/></a>
+                    </div>
                 </div>
                 
             </footer>
