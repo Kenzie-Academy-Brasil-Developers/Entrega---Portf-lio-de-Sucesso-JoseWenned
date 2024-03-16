ad5e309b2cf034b23c18889afee5ee8ee160f14b
JoseWenned <118996462+JoseWenned@users.noreply.github.com>
2024-03-16 03:33:40 -0300

    feat: style component listProjects

--- src/Components/MyProjects/myProjectsFrontend.component.tsx
+++ src/Components/MyProjects/myProjectsFrontend.component.tsx
@@ -1,6 +1,7 @@
 import { useContext } from "react"
 import { UserContext } from "../../Providers/userContext.provider"
 import { ListProjectsAll } from "./ListProjectsAll/listProjects.component"
+import  style  from "./style.module.scss"
 
 export const MyProjects = () => {
 
@@ -8,20 +9,22 @@ export const MyProjects = () => {
 
     return(
         <>  
-            <header>
+            <section className={style.sectionPrimary}>
+                <div className={style.containerButton}>
+                        <button className={style.button} type="submit" onClick={() => setCategory("All projects")}>All projects</button>
+                        <button className={style.button} type="submit" onClick={() => setCategory("Frontend")}>Frontend projects</button>
+                        <button className={style.button} type="submit" onClick={() => setCategory("Backend")}>Backend projects</button>
+                        <button className={style.button} type="submit" onClick={() => setCategory("Fullstack")}>Fullstack projects</button>
+                </div>
+            </section>
+            <section className={style.sectionSecond}>
 
-                <button type="submit" onClick={() => setCategory("All projects")}>all projects</button>
-                <button type="submit" onClick={() => setCategory("Frontend")}>Frontend projects</button>
-                <button type="submit" onClick={() => setCategory("Backend")}>Backend projects</button>
-                <button type="submit" onClick={() => setCategory("Fullstack")}>Fullstack projects</button>
-                
-            </header>
-            <section>
+                <h2 className={style.titleProjects}>My Projects</h2>
 
-                <h2>My Projects</h2>
+                <div className={style.sectionSubSecond}>
 
-                <div>
                     <ListProjectsAll/>
+                    
                 </div>
                 
             </section>
