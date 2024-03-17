1654764daa7702ebc075852e82f50d47a1ec8a61
JoseWenned <118996462+JoseWenned@users.noreply.github.com>
2024-03-17 09:45:48 -0300

    feat: project official add in database.js

--- src/Data/database.tsx
+++ src/Data/database.tsx
@@ -2,33 +2,14 @@ export const projects = [
 
     {
         id: 1,
-        name: "Projeto 1",
-        language: "Javascript",
-        description: "Description project...",
-        linkGitHub: "link 1",
+        image: "./src/assets/Projeto M1 - E-commerce de vestuário.png",
+        name: "Projeto M1 - E-commerce de vestuário",
+        language: "Html, Css3, Javascript",
+        description: "O projeto Front End se resume à criação de um layout de E-commerce baseado em um guia de estilo. O estilo obrigatório nesse sentido envolve a criação de novos cartões para o cenário de vendas, acoplar produtos ao carrinho, remover produtos do carrinho e a contagem de unidades totais e preço. Ambas as navegações superiores e barra de pesquisa devem funcionar, permitindo o filtro sobre as categorias selecionadas ou pesquisadas.",
+        linkGitHub: "https://github.com/Kenzie-Academy-Brasil-Developers/M1-Ecommerce-JoseWenned",
         linkVercel: "link 2",
         category: "Frontend"
     },
-
-    {
-        id: 2,
-        name: "Projeto 2",
-        language: "Typescript",
-        description: "Description project...",
-        linkGitHub: "link 1",
-        linkVercel: "link 2",
-        category: "Backend"
-    },
-
-    {
-        id: 3,
-        name: "Projeto 3",
-        language: "react",
-        description: "Description project...",
-        linkGitHub: "link 1",
-        linkVercel: "link 2",
-        category: "Fullstack"
-    }
     
 ];
 
