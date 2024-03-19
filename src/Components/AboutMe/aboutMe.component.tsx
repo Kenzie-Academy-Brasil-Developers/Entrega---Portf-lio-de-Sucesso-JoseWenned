4c0a4bef08833b97bcaab8b953292609298e5769
JoseWenned <118996462+JoseWenned@users.noreply.github.com>
2024-03-19 04:15:30 -0300

    feat: add information user in page about me

--- src/Components/AboutMe/aboutMe.component.tsx
+++ src/Components/AboutMe/aboutMe.component.tsx
@@ -1,18 +1,64 @@
+import { FaUser } from "react-icons/fa";
+import { RiGraduationCapFill } from "react-icons/ri";
+import { MdEmail } from "react-icons/md";
+import { FaHouseUser } from "react-icons/fa";
+
 export const AboutMe = () => {
     return(
         <>
             <section>
-                <div>
-                    <img src="" alt=""/>
-                </div>
-                <div>
-                    <h1>Hello!</h1>
-                    <h2>I'm José Wenned.</h2>
-
-                    <p>Descrição</p>
-                </div>
 
                 <div>
+
+                    <h1>About me</h1>
+
+                    <p>Quality of services for over 1 year</p>
+
+                    <p>Driven by an insatiable curiosity, I dedicate myself to the craft of coding, engaging in new projects and continually learning.</p>
+                    
+                    <div>
+
+                        <FaUser />
+                        <p>José Wenned</p>
+
+                    </div>
+
+                    <div>
+
+                        <RiGraduationCapFill />
+                        <p>Develop full stack</p>
+
+                    </div>
+
+                    <div>
+
+                        <MdEmail />
+                        <p>wennedchaves1997@gmail.com</p>
+
+                    </div>
+
+                    <div>
+
+                        <FaHouseUser />
+                        <p>Quixeré, Ceará, Brasil</p>
+                        
+                    </div>
+
+                    <div>
+
+                        <h3>Language skills</h3>
+
+                        <p>Portuguese(Native)</p>
+                        <p>English(basic)</p>
+
+                    </div>
+
+                </div>
+
+                <div>
+
+                    <h2>Skills</h2>
+
                     <img src="./src/assets/css-3.png" alt="icon.css.png"/>
                     <img src="./src/assets/database.png" alt="icon.database.png"/>
                     <img src="./src/assets/Express.png" alt="icon.express.png"/>
@@ -21,7 +67,9 @@ export const AboutMe = () => {
                     <img src="./src/assets/python.png" alt="icon.python.png"/>
                     <img src="./src/assets/physics.png" alt="icon.physics.png"/>
                     <img src="./src/assets/typescrit.png" alt="icon.typescrit.png"/>
+
                 </div>
+
             </section>
         </>
     )
