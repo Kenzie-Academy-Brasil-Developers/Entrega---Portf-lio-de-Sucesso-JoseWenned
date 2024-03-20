85106d7cc6da269f7941c410bd7de69559674594
JoseWenned <118996462+JoseWenned@users.noreply.github.com>
2024-03-20 08:27:04 -0300

    feat: add new image icons languange database

--- src/Data/database.tsx
+++ src/Data/database.tsx
@@ -98,42 +98,107 @@ export const skills = [
 
     {   
         id: 1,
-        image: "./src/assets/css-3.png"
+        image: "./src/assets/icons8-html-48.png"
     },
 
     {   
         id: 2,
-        image: "./src/assets/database.png"
+        image: "./src/assets/icons8-css-48.png"
     },
 
     {
         id: 3,
-        image: "./src/assets/Express.png"
+        image: "./src/assets/scss-svgrepo-com.png"
     },
 
     {
         id: 4,
-        image: "./src/assets/html.png"
+        image: "./src/assets/tailwindcss-icon-svgrepo-com.png"
     },
 
     {
         id: 5,
-        image: "./src/assets/nodejs.png"
+        image: "./src/assets/file-type-styled.png"
     },
 
     {
         id: 6,
-        image: "./src/assets/python.png"
-    },
-    
-    {
-        id: 7,
-        image: "./src/assets/physics.png"
+        image: "./src/assets/icons8-javascript-48.png"
     },
 
     {
+        id: 7,
+        image: "./src/assets/icons8-texto-datilografado-48.png"
+    },
+    
+    {
         id: 8,
-        image: "./src/assets/typescrit.png"
+        image: "./src/assets/react-javascript-js-framework-facebook-svgrepo-com.png"
+    },
+
+    {
+        id: 9,
+        image: "./src/assets/node-js-svgrepo-com.png"
+    },
+
+    {
+        id: 10,
+        image: "./src/assets/express-svgrepo-com.png"
+    },
+
+    {
+        id: 11,
+        image: "./src/assets/icons8-python-48.png"
+    },
+
+    {
+        id: 12,
+        image: "./src/assets/django-svgrepo-com.png"
+    },
+
+    {
+        id: 13,
+        image: "./src/assets/django-svgrepo-com.png"
+    },
+
+    {
+        id: 14,
+        image: "./src/assets/postgresql-svgrepo-com.png"
+    },
+
+    {
+        id: 15,
+        image: "./src/assets/mongodb-svgrepo-com.png"
+    },
+
+    {
+        id: 16,
+        image: "./src/assets/sqlite-svgrepo-com.png"
+    },
+
+    {
+        id: 17,
+        image: "./src/assets/docker-svgrepo-com.png"
+    },
+
+    {
+        id: 18,
+        image: "./src/assets/git-svgrepo-com.png"
+    },
+
+    {
+        id: 19,
+        image: "./src/assets/github-color-svgrepo-com.png"
+    },
+
+    {
+        id: 20,
+        image: "./src/assets/figma-svgrepo-com.png"
+    },
+
+    {
+        id: 21,
+        image: "./src/assets/figma-svgrepo-com.png"
     },
 
 ]
@@ -142,7 +207,7 @@ export const tools = [
 
     {
         id: 1,
-        image: "./src/assets/icons8-windown-10-80.png"
+        image: "./src/assets/icons8-windows-10-80.png"
     },
 
     {
@@ -157,12 +222,17 @@ export const tools = [
 
     {
         id: 4,
-        image: "./src/assets/insomnia-svgrepo-com.png"
+        image: "./src/assets/Insomnia.png"
     },
 
     {
         id: 5,
-        image: "./src/assets/trellor-color-svgrepo-com.png"
+        image: "./src/assets/jira-svgrepo-com.png"
+    },
+
+    {
+        id: 6,
+        image: "./src/assets/trello-color-svgrepo-com.png"
     },
 
 ]
