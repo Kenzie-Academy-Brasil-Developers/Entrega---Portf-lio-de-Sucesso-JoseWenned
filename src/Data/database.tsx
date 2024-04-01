49c93c79e021b86f52ea1534b4aec007c84bf3a8
unknown <wennedchaves1997@gmail.com>
2024-04-01 10:51:47 -0300

    feat: clear database dados

--- src/Data/database.tsx
+++ src/Data/database.tsx
@@ -13,92 +13,16 @@ export const projects = [
         linkVercel: "link 2",
         category: "Frontend"
     },
-
-    {
-        id: 1,
-        image: "./src/assets/Projeto M1 - E-commerce de vestuário.png",
-        name: "Projeto M1 - E-commerce de vestuário",
-        language: "Html, Css3, Javascript",
-        description: `O projeto Front End se resume à criação de um layout de E-commerce baseado em um guia de estilo. 
-        O estilo obrigatório nesse sentido envolve a criação de novos cartões para o cenário de vendas, acoplar produtos ao carrinho, 
-        remover produtos do carrinho e a contagem de unidades totais e preço. Ambas as navegações superiores e barra de pesquisa devem funcionar,
-        permitindo o filtro sobre as categorias selecionadas ou pesquisadas.`,
-        linkGitHub: "https://github.com/Kenzie-Academy-Brasil-Developers/M1-Ecommerce-JoseWenned",
-        linkVercel: "link 2",
-        category: "Frontend"
-    },
-
-    {
-        id: 1,
-        image: "./src/assets/Projeto M1 - E-commerce de vestuário.png",
-        name: "Projeto M1 - E-commerce de vestuário",
-        language: "Html, Css3, Javascript",
-        description: `O projeto Front End se resume à criação de um layout de E-commerce baseado em um guia de estilo. 
-        O estilo obrigatório nesse sentido envolve a criação de novos cartões para o cenário de vendas, acoplar produtos ao carrinho, 
-        remover produtos do carrinho e a contagem de unidades totais e preço. Ambas as navegações superiores e barra de pesquisa devem funcionar,
-        permitindo o filtro sobre as categorias selecionadas ou pesquisadas.`,
-        linkGitHub: "https://github.com/Kenzie-Academy-Brasil-Developers/M1-Ecommerce-JoseWenned",
-        linkVercel: "link 2",
-        category: "Frontend"
-    },
     
 ];
 
 export const experiences = [
 
-    {   
-        id: 1,
-        company: "Kilpolpas",
-        office: "Develop full stack",
-        date: "jan 2023 - jan 1024"
-    },
-
-    {   
-        id: 1,
-        company: "Kilpolpas",
-        office: "Develop full stack",
-        date: "jan 2023 - jan 1024"
-    },
-
-    {   
-        id: 1,
-        company: "Kilpolpas",
-        office: "Develop full stack",
-        date: "jan 2023 - jan 1024"
-    },
-
-    {   
-        id: 1,
-        company: "Kilpolpas",
-        office: "Develop full stack",
-        date: "jan 2023 - jan 1024"
-    },
-
+   
 
 ]
 
 export const educations = [
-
-    {   
-        id: 1,
-        intitution: "Kenzie Academy Brasil",
-        training: "Develop full stack",
-        date: "jan 2023 - jan 2024"
-    },
-
-    {   
-        id: 1,
-        intitution: "Kenzie Academy Brasil",
-        training: "Develop full stack",
-        date: "jan 2023 - jan 2024"
-    },
-
-    {   
-        id: 1,
-        intitution: "Kenzie Academy Brasil",
-        training: "Develop full stack",
-        date: "jan 2023 - jan 2024"
-    },
 
     {   
         id: 1,
