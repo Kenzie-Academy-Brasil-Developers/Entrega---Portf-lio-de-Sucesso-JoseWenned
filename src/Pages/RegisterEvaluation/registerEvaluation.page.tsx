8c436e18fb26b1cbf3da03cca852a9af3d05f8a7
unknown <wennedchaves1997@gmail.com>
2024-07-11 06:30:01 -0300

    feat: creating page register evaluation

---
+++ src/Pages/RegisterEvaluation/registerEvaluation.page.tsx
@@ -0,0 +1,21 @@
+import { Footer } from "../../Components/Footer/footer.component"
+import { FormRegister } from "../../Components/FormRegister/formRegiter.component"
+import { Header } from "../../Components/Header/header.component"
+
+export const RegisterEvaluation = () => {
+
+    return (
+
+        <>
+        
+            <Header/>
+
+            <FormRegister/>
+
+            <Footer/>
+
+        </>
+
+    )
+
+}
