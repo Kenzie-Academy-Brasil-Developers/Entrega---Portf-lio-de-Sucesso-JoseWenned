705fc7427c5daa8261417b66e4bbe69021be38e1
unknown <wennedchaves1997@gmail.com>
2024-07-11 19:12:11 -0300

    feat: interfaces create and read evaluation creating

--- src/Interfaces/EvaluationContext/evaluation.interface.tsx
+++ src/Interfaces/EvaluationContext/evaluation.interface.tsx
@@ -1,3 +1,17 @@
+import { Value } from "sass";
+
+export interface IEvaluation {
+
+    id: string
+    name: string;   
+    profession: string;
+    description: string;
+
+}
+
+export interface IEvaluationCreate extends Omit< Value, "id" >{};
+export type IEvaluationReturn = IEvaluationCreate[];
+
 export interface IEvaluationProvidersProps {
 
     children: React.ReactNode;
